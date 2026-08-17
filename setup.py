891422ed776d42ba84cde1609a03d0d0862905fe
Anonymous User <21244564+AmandaSauber@users.noreply.github.com>
2026-08-17 12:26:58 -0400

    updated per Sam's feedback. Updated spacing on template in Signature.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.DemandForPaymentLetter',
       license='',
       url='https://courtformsonline.org',
       packages=find_namespace_packages(),
-      install_requires=['docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@fix_session_query', 'docassemble.GithubFeedbackForm @ git+https://github.com/SuffolkLITLab/docassemble-GithubFeedbackForm.git@main'],
+      install_requires=['docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.GithubFeedbackForm @ git+https://github.com/SuffolkLITLab/docassemble-GithubFeedbackForm.git@main'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/DemandForPaymentLetter/', package='docassemble.DemandForPaymentLetter'),
      )
